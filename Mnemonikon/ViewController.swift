0fe676fbc06b6347e117ad161b2b04c1cfa8573e
Igor_Potemkin <potemkinigor@yandex.ru>
2020-12-22 18:44:08 +0300

    Almost first version

--- Mnemonikon/ViewController.swift
+++ Mnemonikon/ViewController.swift
@@ -17,7 +17,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var RandomImage2: UIImageView!
     @IBOutlet weak var RandomImage3: UIImageView!
     @IBOutlet weak var RandomImage4: UIImageView!
-    @IBOutlet weak var userAnswerText: UITextField!
     @IBOutlet weak var correctUnswersCountLabel: UILabel!
     
 
@@ -26,8 +25,7 @@ class ViewController: UIViewController {
     var numberAndImageDict: [Int : String] = [:]
     var userCorrentAnswersCount = 0
     let numbersLimit = 99
-    
-    // x, y, width, height
+    // x, y, width, height:
     var picturesCoordinatesArray = [(20, 140, 183, 142),
                                      (211, 140, 183, 142),
                                      (20, 345, 183, 142),
@@ -36,12 +34,9 @@ class ViewController: UIViewController {
     var picture2CoordinatesArray = [Int]()
     var picture3CoordinatesArray = [Int]()
     var picture4CoordinatesArray = [Int]()
-    let imageViewsArray: [UIImageView] = []
-    
-    
-    
-    let correctAnswersDict = [0: "Номерок", 1: "Еж", 2: "Яд", 3: "Ухо", 4: "Чай", 5: "Обои", 6: "Юла", 7: "Оса", 8: "Ива", 9: "Яйцо", 10: "Огонь", 11: "Гагарин", 12: "Гитара", 13: "Гайка"]
-
+    var imageViewsArray: [UIImageView] = []
+    var correctPictureNumber = 0
+    var userAnswer = 0
     
     func updatePictures () {
         
@@ -55,14 +50,26 @@ class ViewController: UIViewController {
             randomNumber = arc4random_uniform(UInt32(numbersLimit))
         }
         
+        RandomNumberLabel.text = String(numberAndImageDict.randomElement()!.key)
+        
         var i = 0
         for (_, picture) in numberAndImageDict {
             imageViewsArray[i].image = UIImage(named: picture)
+            
+            if RandomNumberLabel.text! == picture.dropLast(5) {
+                correctPictureNumber = i + 1
+            }
+            
             i += 1
         }
         
-        RandomNumberLabel.text = String(numberAndImageDict.randomElement()!.key)
-    
+        i = 0
+        
+        for element in imageViewsArray {
+            element.frame = CGRect(x: picturesCoordinatesArray[i].0, y: picturesCoordinatesArray[i].1, width: picturesCoordinatesArray[i].2, height: picturesCoordinatesArray[i].3)
+            i += 1
+        }
+        
     }
     
     
@@ -80,6 +87,7 @@ class ViewController: UIViewController {
         RandomImage3.addGestureRecognizer(tapGestureRecognizer3)
         RandomImage4.addGestureRecognizer(tapGestureRecognizer4)
         
+        
         updatePictures()
         
     }
@@ -92,18 +100,17 @@ class ViewController: UIViewController {
         if Int(RandomImage1.frame.width) == picturesCoordinatesArray[0].2 && Int(RandomImage1.frame.height) == picturesCoordinatesArray[0].3 {
             newHeight = picturesCoordinatesArray[0].3 * 7 / 5
             newWidth = picturesCoordinatesArray[0].2 * 7 / 5
+            
+            makeOtherPicturesSmallAgain(picture: RandomImage1)
+                        
         } else {
             newHeight = picturesCoordinatesArray[0].3
             newWidth = picturesCoordinatesArray[0].2
         }
         
         RandomImage1.frame = CGRect(x: picturesCoordinatesArray[0].0, y: picturesCoordinatesArray[0].1, width: newWidth, height: newHeight)
+        userAnswer = 1
         
-        for element in imageViewsArray {
-            if element != RandomImage1 {
-                makeOtherPicturesSmallAgain(picture: element, x: , y: <#T##Int#>, width: <#T##Int#>, height: <#T##Int#>)
-            }
-        }
 
     }
     
@@ -114,12 +121,16 @@ class ViewController: UIViewController {
         if Int(RandomImage2.frame.width) == picturesCoordinatesArray[1].2 && Int(RandomImage2.frame.height) == picturesCoordinatesArray[1].3 {
             newHeight = picturesCoordinatesArray[1].3 * 7 / 5
             newWidth = picturesCoordinatesArray[1].2 * 7 / 5
+            
+            makeOtherPicturesSmallAgain(picture: RandomImage2)
+            
         } else {
             newHeight = picturesCoordinatesArray[1].3
             newWidth = picturesCoordinatesArray[1].2
         }
         
         RandomImage2.frame = CGRect(x: picturesCoordinatesArray[1].0, y: picturesCoordinatesArray[1].1, width: newWidth, height: newHeight)
+        userAnswer = 2
     }
     
     @ objc func didTapImageView3(_ sender: UITapGestureRecognizer) {
@@ -129,12 +140,16 @@ class ViewController: UIViewController {
         if Int(RandomImage3.frame.width) == picturesCoordinatesArray[2].2 && Int(RandomImage3.frame.height) == picturesCoordinatesArray[2].3 {
             newHeight = picturesCoordinatesArray[2].3 * 7 / 5
             newWidth = picturesCoordinatesArray[2].2 * 7 / 5
+            
+            makeOtherPicturesSmallAgain(picture: RandomImage3)
+            
         } else {
             newHeight = picturesCoordinatesArray[2].3
             newWidth = picturesCoordinatesArray[2].2
         }
         
         RandomImage3.frame = CGRect(x: picturesCoordinatesArray[2].0, y: picturesCoordinatesArray[2].1, width: newWidth, height: newHeight)
+        userAnswer = 3
     }
     
     @ objc func didTapImageView4(_ sender: UITapGestureRecognizer) {
@@ -144,31 +159,39 @@ class ViewController: UIViewController {
         if Int(RandomImage4.frame.width) == picturesCoordinatesArray[3].2 && Int(RandomImage4.frame.height) == picturesCoordinatesArray[3].3 {
             newHeight = picturesCoordinatesArray[3].3 * 7 / 5
             newWidth = picturesCoordinatesArray[3].2 * 7 / 5
+            
+            makeOtherPicturesSmallAgain(picture: RandomImage4)
+            
         } else {
             newHeight = picturesCoordinatesArray[3].3
             newWidth = picturesCoordinatesArray[3].2
         }
         
         RandomImage4.frame = CGRect(x: picturesCoordinatesArray[3].0, y: picturesCoordinatesArray[3].1, width: newWidth, height: newHeight)
+        userAnswer = 4
     }
     
-    func makeOtherPicturesSmallAgain (picture: UIImageView, x: Int, y: Int, width: Int, height: Int) {
-        picture.frame = CGRect(x: x, y: y, width: width, height: height)
+    func makeOtherPicturesSmallAgain (picture: UIImageView) {
+        
+        var i = 0
+        for element in imageViewsArray {
+            if element != picture {
+                element.frame = CGRect(x: picturesCoordinatesArray[i].0, y: picturesCoordinatesArray[i].1, width: picturesCoordinatesArray[i].2, height: picturesCoordinatesArray[i].3)
+                i += 1
+            } else {
+                i += 1
+            }
+        }
     }
     
     @IBAction func CheckUnswer(_ sender: Any) {
-        
-        let userAnswer = userAnswerText.text
-        
-        let correctAnswer = correctAnswersDict[Int(RandomNumberLabel.text!)!]
-        
-        if userAnswer == correctAnswer {
-            userAnswerText.text = "Правильно!"
+             
+        if userAnswer == correctPictureNumber {
+            
             userCorrentAnswersCount += 1
-            correctUnswersCountLabel.text = String(userCorrentAnswersCount)
-        } else {
-            userAnswerText.text = "Правильный ответ - \(String(describing: correctAnswersDict[Int(RandomNumberLabel.text!)!])) "
         }
+        
+        correctUnswersCountLabel.text = String(userCorrentAnswersCount)
         
         updatePictures()
         
